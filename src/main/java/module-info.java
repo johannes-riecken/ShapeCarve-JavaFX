0abef0c2e19c1cbd0639807f614d700897570073
Johannes Riecken <johannes.riecken@gmail.com>
2024-05-28 16:33:02 +0200

    Use FXyz to support mouse-based camera manipulation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.shapecarvejavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.fxyz3d.core;
 
 
     opens org.example.shapecarvejavafx to javafx.fxml;
